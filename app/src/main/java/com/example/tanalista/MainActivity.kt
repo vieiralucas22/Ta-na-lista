32afed8faa250653598ae9de50bcc5a96ca4ce19
vieiralucas22 <vieiralucas2205@gmail.com>
2025-11-02 18:27:15 -0300

    Create a basic suite navigation

--- app/src/main/java/com/example/tanalista/MainActivity.kt
+++ app/src/main/java/com/example/tanalista/MainActivity.kt
@@ -4,15 +4,79 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteDefaults
+import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteScaffold
+import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteScaffoldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewScreenSizes
+import com.example.tanalista.ui.theme.ButtonBackground
 import com.example.tanalista.ui.views.CartView
+import com.example.tanalista.ui.views.ChatView
 import com.example.tanalista.ui.views.LoginView
+import com.example.tanalista.ui.views.ProfileView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            CartView()
+
+            MyApplicationApp()
         }
     }
+}
+
+@PreviewScreenSizes
+@Composable
+fun MyApplicationApp() {
+    var currentDestination by rememberSaveable { mutableStateOf(AppDestinations.HOME) }
+
+    NavigationSuiteScaffold(
+        navigationSuiteItems = {
+            AppDestinations.entries.forEach {
+                item(
+                    icon = {
+                        Icon(
+                            painter = painterResource(it.resourceId),
+                            contentDescription = it.label
+                        )
+                    },
+                    label = { Text(it.label) },
+                    selected = it == currentDestination,
+                    onClick = { currentDestination = it }
+
+                )
+            }
+        }
+    ) {
+        when (currentDestination)
+        {
+            AppDestinations.HOME -> CartView()
+            AppDestinations.CHAT -> ChatView()
+            AppDestinations.PROFILE -> ProfileView()
+        }
+    }
+}
+
+enum class AppDestinations(
+    val label: String,
+    val resourceId: Int,
+) {
+    CHAT("Chat", R.drawable.ic_chat),
+    HOME("Home", R.drawable.ic_home),
+    PROFILE("Profile", R.drawable.ic_profile),
 }
---
+++ app/src/main/java/com/example/tanalista/ui/views/ChatView.kt
@@ -0,0 +1,34 @@
+package com.example.tanalista.ui.views
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.tanalista.ui.theme.BackgroundColor
+
+@Composable
+fun ChatView() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(BackgroundColor)
+            .padding(20.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+    ) {
+        Text(text = "Chat")
+    }
+}
+
+@Preview
+@Composable
+fun ChatViewPreview() {
+    ChatView()
+}
