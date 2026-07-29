069be801ce1d6cd44ef6ff467d38966d3b1346c6
vieiralucas22 <vieiralucas2205@gmail.com>
2026-07-28 22:05:49 -0300

    Update constant file to match wityh kotlin pattern

--- app/src/main/java/com/example/tanalista/constants/database/DatabaseConstants.kt
+++ app/src/main/java/com/example/tanalista/constants/database/DatabaseConstants.kt
@@ -1,25 +1,22 @@
 package com.example.tanalista.constants.database
 
-class DatabaseConstants {
+object DatabaseConstants {
 
-    companion object
-    {
-        /// General
-        const val ID = "id"
-        const val DATABASE_NAME = "ApplicationDB2"
+    // General
+    const val ID = "id"
+    const val DATABASE_NAME = "ApplicationDB2"
 
-        /// Product List Entity
-        const val PRODUCT_LIST_TABLE_NAME = "ProductList"
-        const val LIST_PRIMARY_KEY_NAME = "listId"
-        const val PRODUCT_PRIMARY_KEY_NAME = "productId"
+    // Product List Entity
+    const val PRODUCT_LIST_TABLE_NAME = "ProductList"
+    const val LIST_PRIMARY_KEY_NAME = "listId"
+    const val PRODUCT_PRIMARY_KEY_NAME = "productId"
 
-        /// Product Entity
-        const val PRODUCT_TABLE_NAME = "Product"
+    // Product Entity
+    const val PRODUCT_TABLE_NAME = "Product"
 
-        /// Product Category Entity
-        const val PRODUCT_CATEGORY_TABLE_NAME = "ProductCategory"
+    // Product Category Entity
+    const val PRODUCT_CATEGORY_TABLE_NAME = "ProductCategory"
 
-        /// List Entity
-        const val LIST_TABLE_NAME = "List"
-    }
+    // List Entity
+    const val LIST_TABLE_NAME = "List"
 }
